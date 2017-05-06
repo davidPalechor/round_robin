3f63515454c5984847979a141ff0c6ad15b97e17
Sergio Palechor <Sergio Palechor>
2017-05-06 14:25:04 -0500

    promesas

--- cliente/src/app/roundrobin.service.ts
+++ cliente/src/app/roundrobin.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class RoundrobinService {
 	private apiURL = 'localhost:8000/round_robin/'
-  constructor() { }
+  
+  constructor(private http:Http) { }
 
+  getInfoHilos(){
+  	return this.http.get(this.apiURL)
+  		.toPromise()
+  		.then(response => response.json())
+  		.catch(this.handleError);
+  }
+
+  private handleError(error: any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
 }
--- mysite/RR/urls.py
+++ mysite/RR/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-        url(r'^$', views.index),
         url(r'round_robin', views.round_robin),
         url(r'fifo', views.fifo),
     ]
